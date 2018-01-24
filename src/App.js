3115a9fb38e7a739c721a66587df69af22a5e4fc
Beatriz da Silveira <beatriz1304@gmail.com>
2018-01-23 22:01:25 -0200

    Adds props to a Person component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person />
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
     );
   }
