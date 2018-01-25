26afddc45b47c357b3a41898db2336db54c1d465
Beatriz da Silveira <beatriz1304@gmail.com>
2018-01-25 00:00:23 -0200

    Renders Person components on a more elegant way

--- src/App.js
+++ src/App.js
@@ -45,6 +45,26 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNamehandler.bind(this, 'Claudia')}
+            changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}/>
+        </div>
+      );
     }
 
     return (
@@ -53,21 +73,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonHandler}>Toggle Persons</button>
-          {this.state.showPersons ?
-            <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNamehandler.bind(this, 'Claudia')}
-              changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}/>
-          </div> : null
-        }
+        {persons}
       </div>
     );
   }
