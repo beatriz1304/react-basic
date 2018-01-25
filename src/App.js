4e2e9333298b091954100a3c559365d895792c7c
Beatriz da Silveira <beatriz1304@gmail.com>
2018-01-24 22:33:47 -0200

    Adds an inline style for a button

--- src/App.js
+++ src/App.js
@@ -32,10 +32,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={() => this.switchNamehandler('Biaaa!!!')}>Switch Name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNamehandler('Biaaa!!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
