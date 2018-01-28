e166684b1860717bb31154107f6e6a9899a15166
Beatriz da Silveira <beatriz1304@gmail.com>
2018-01-27 22:10:41 -0200

    Implements an efficient way of updating person name

--- src/App.js
+++ src/App.js
@@ -13,14 +13,21 @@ class App extends Component {
     showPersons: false,
   }
 
-  nameChangedHandler = event => {
-    this.setState({
-      persons:[
-        { name: 'Bia', age: 25 },
-        { name: event.target.value, age: 25 },
-        { name: 'Yasmin', age: 15 },
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -53,7 +60,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );
