47f520f6a01904c10980c667854da73c8a7856c7
Beatriz da Silveira <beatriz1304@gmail.com>
2018-02-02 18:48:05 -0200

    Fixes parameter value in person input event handler

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -42,7 +42,7 @@ class Persons extends PureComponent {
         name={person.name}
         age={person.age}
         key={person.id}
-        changed={(event) => this.props.changed(event, person)}/>
+        changed={(event) => this.props.changed(event, person.id)}/>
     });
   }
 }
